bfbd12e44da7d06e9be344d43a4b499521d5a67f
wenchangliu <wenchangliu@google.com>
2019-03-25 19:05:52 +0800

    sanders: Enable QC2 bufferqueue block-pool
    Enable Qualcomm codec 2.0 bufferqueue block-pool for
    reducing 10% of power consumption on 1080p@30fps video playback
    
    Test: Full video manual test
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -155,6 +155,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.display.disable_skip_validate=1
 
+# Enable QC2 bufferqueue block-pool
+PRODUCT_PROPERTY_OVERRIDES += vendor.qc2.use.bqpool=1
+
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
