1f5075ed4919e8638a4f25a14965058dd5f45697
Mayur <ultramayur123@gmail.com>
2020-02-19 13:06:04 +0000

    Sanders: prebuilt: override messaging with SmsOrganizer
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -492,8 +492,9 @@ PRODUCT_PACKAGES += \
 # Prebuilt Packages
 PRODUCT_PACKAGES += \
     Via \
-    Wallpapers
-    
+    Wallpapers \
+    SmsOrganizer
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
